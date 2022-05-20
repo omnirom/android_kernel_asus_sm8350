4dd05061b5d8819c1239cc90d7cc399285caa9e2
dmd79 <davidevinavil@gmail.com>
2022-05-20 11:32:42 +0200

    sm8350: Make regenerate Asus defconfig more generic
    Change-Id: I062330355518056f76aeb35bfce429dd06adcca8

--- scripts/gki/regenerate_defconfig.sh
+++ scripts/gki/regenerate_defconfig.sh
@@ -4,16 +4,19 @@ echo
 echo "Regenerate Asus QGKI Defconfig"
 echo
 
-export KERN_SRC=$HOME/omni/kernel/asus/sm8350
+SOURCE_DIR=$(realpath ../../../../../)
+echo $SOURCE_DIR
+
+export KERN_SRC=$SOURCE_DIR/kernel/asus/sm8350
 export ONLY_GKI=0
 export ARCH=arm64
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$HOME/omni/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export HOSTAR=$HOME/omni/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/llvm-ar
-export HOSTCC=$HOME/omni/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/clang
-export HOSTLD=$HOME/omni/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/ld.lld
-export LD=$HOME/omni/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/ld.lld
-export REAL_CC=$HOME/omni/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/clang
+export CROSS_COMPILE=$SOURCE_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export HOSTAR=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/llvm-ar
+export HOSTCC=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/clang
+export HOSTLD=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/ld.lld
+export LD=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/ld.lld
+export REAL_CC=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/clang
 export TARGET_BUILD_VARIANT=user
 export ASUS_BUILD_PROJECT=SAKE
 
