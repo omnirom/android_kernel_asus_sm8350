7b1cbea546b5b50e3905b4a9bf0ebf4207b8a09e
micky387 <mickaelsaibi@free.fr>
2022-10-29 10:32:39 +0200

    sm8350: Edit Clang path in regenerate_defconfig.sh
    Change-Id: Ied0d30ca39148b791eed981bcdc21492eb9f60e0

--- scripts/gki/regenerate_defconfig.sh
+++ scripts/gki/regenerate_defconfig.sh
@@ -12,11 +12,11 @@ export ONLY_GKI=0
 export ARCH=arm64
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=$SOURCE_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export HOSTAR=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/llvm-ar
-export HOSTCC=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/clang
-export HOSTLD=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/ld.lld
-export LD=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/ld.lld
-export REAL_CC=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r416183b1/bin/clang
+export HOSTAR=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-ar
+export HOSTCC=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r450784e/bin/clang
+export HOSTLD=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r450784e/bin/ld.lld
+export LD=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r450784e/bin/ld.lld
+export REAL_CC=$SOURCE_DIR/prebuilts/clang/host/linux-x86/clang-r450784e/bin/clang
 export TARGET_BUILD_VARIANT=user
 export ASUS_BUILD_PROJECT=SAKE
 
