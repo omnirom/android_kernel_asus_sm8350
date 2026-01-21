036c21e1336414897883790641326691706e5640
micky387 <mickaelsaibi@free.fr>
2026-01-21 15:34:00 -0500

    nfc: Import Asus ese driver
    Change-Id: I602e0030b9cb7dcb3842ea6dc98eeb22ce9bd679
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1307,8 +1307,9 @@ int do_send_sig_info(int sig, struct kernel_siginfo *info, struct task_struct *p
  * We don't want to have recursive SIGSEGV's etc, for example,
  * that is why we also clear SIGNAL_UNKILLABLE.
  */
-static int
-force_sig_info_to_task(struct kernel_siginfo *info, struct task_struct *t)
+ 
+/*Asus BSP*/
+int force_sig_info_to_task(struct kernel_siginfo *info, struct task_struct *t)
 {
 	unsigned long int flags;
 	int ret, blocked, ignored;
