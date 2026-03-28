615556731cd1c23a872fb02613ba3fe827fad288
micky387 <mickaelsaibi@free.fr>
2026-03-27 23:26:12 -0400

    asus_cam_sensor: Fix sscanf overflow error
    ../../../../../../kernel/asus/sm8350/techpack/camera/drivers/cam_sensor_module/cam_sensor/asus_cam_sensor.c:1238:30: error: 'sscanf' may overflow; destination buffer in argument 3 has size 24, but the corresponding specifier may require s
    ize 25 [-Werror,-Wfortify-source]
     1238 |         sscanf(buf_cali_id, "%24s", buf_cali_id_tmp);
          |                                     ^
    1 error generated.
    
    Change-Id: I7c1ed6e0dc7149b1f5b2edb29d2ae2b69394a91f
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- techpack/camera/drivers/cam_sensor_module/cam_sensor/asus_cam_sensor.c
+++ techpack/camera/drivers/cam_sensor_module/cam_sensor/asus_cam_sensor.c
@@ -1180,8 +1180,8 @@ static void compareCameraSN (uint8_t moduleGroupIndex)
 	int SN_Offset=0;
 
 	static uint8_t buf_cali_id[128];
-	static uint16_t buf_cali_id_tmp[OTP_ID_LEN];
-	static uint8_t otp_data[OTP_ID_LEN];
+	static uint16_t buf_cali_id_tmp[OTP_ID_LEN +1];
+	static uint8_t otp_data[OTP_ID_LEN +1];
 
 	struct file *fp;
 	loff_t pos = 0;
